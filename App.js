7003e599a62262c56e1b3ffdbc14d402408b27fe
petercolella <peter.joseph.colella@gmail.com>
2020-06-27 20:34:30 -0400

    updates App.js with navigation flow

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomNavigator } from 'react-navigation-tabs';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -9,19 +9,21 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
-const navigator = createStackNavigator(
-  {
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
     Signin: SigninScreen,
     Signup: SignupScreen
-  },
-  {
-    initialRouteName: 'Signin',
-    defaultNavigationOptions: {
-      title: 'Tracks'
-    }
-  }
-);
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackDetail: TrackDetailScreen,
+      TrackList: TrackListScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
 
-const App = createAppContainer(navigator);
+const App = createAppContainer(switchNavigator);
 
 export default App;
