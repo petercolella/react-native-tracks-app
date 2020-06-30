b89521193da01b3ed4ae0d4fabf7e3f5c5c6a392
petercolella <peter.joseph.colella@gmail.com>
2020-06-30 18:52:52 -0400

    adds email and password state to SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
       <Spacer>
         <Button title="Sign Up" onPress={() => navigation.navigate('Signin')} />
       </Spacer>
