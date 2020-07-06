2774d5f10f3de3c03849aac4096cf89a5df11f5d
petercolella <peter.joseph.colella@gmail.com>
2020-07-06 00:08:20 -0400

    updates SigninScreen with AuthForm and NavLink

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
-const SigninScreen = ({ navigation }) => {
+const SigninScreen = () => {
+  const { state, signin } = useContext(AuthContext);
   return (
-    <View>
-      <Text>SigninScreen</Text>
-      <Button
-        title="Go to Signup"
-        onPress={() => navigation.navigate('Signup')}
+    <View style={styles.container}>
+      <AuthForm
+        buttonText="Sign In"
+        headerText="Sign In To Your Account"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+      />
+      <NavLink
+        routeName="Signup"
+        text="Don't have an account? Sign up instead."
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    marginTop: 100
+  }
+});
 
 export default SigninScreen;
