7c56add711a2c5ab3a6bb746f46665dd7b13d77f
petercolella <peter.joseph.colella@gmail.com>
2020-07-07 01:34:31 -0400

    adds NavigationEvents component with onWillFocus prop to SigninScreen and SignupScreen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-  const { state, signin } = useContext(AuthContext);
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         buttonText="Sign In"
         headerText="Sign In To Your Account"
