30caac3e2bca8d22fcb64ce32bf3f0221d9521ca
petercolella <peter.joseph.colella@gmail.com>
2020-07-07 01:32:33 -0400

    adds clearErrorMessage dispatch method to AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,11 +7,17 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'clear_error_message':
+      return { ...state, errorMessage: '' };
     case 'signin':
       return { token: action.payload, errorMessage: '' };
     default:
       return state;
   }
+};
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: 'clear_error_message' });
 };
 
 const signup = dispatch => async ({ email, password }) => {
@@ -51,6 +57,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signup, signin, signout },
+  { signup, signin, signout, clearErrorMessage },
   { token: null, errorMessage: '' }
 );
