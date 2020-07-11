568381310bab2619c66e30eb7e0eb6c9325f8221
petercolella <peter.joseph.colella@gmail.com>
2020-07-10 20:00:32 -0400

    adds content and logic to AccountScreen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>AccountScreen</Text>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Account Screen</Text>
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    marginTop: 100
+  }
+});
 
 export default AccountScreen;
