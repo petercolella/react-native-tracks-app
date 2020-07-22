4fd253209780ba83333e7aebb1a46a67145a3089
petercolella <peter.joseph.colella@gmail.com>
2020-07-21 22:13:15 -0400

    adds LocationContext to Map component and TrackCreateScreen

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import MapView, { Polyline } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 37.33233 + i * 0.001,
-      longitude: -122.03121 + i * 0.001
-    });
+  const {
+    state: { currentLocation }
+  } = useContext(LocationContext);
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        ...currentLocation.coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    ></MapView>
   );
 };
 
