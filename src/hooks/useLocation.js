c5de5063777ea16f2d0797eee140f119969c6701
petercolella <peter.joseph.colella@gmail.com>
2020-07-23 15:56:44 -0400

    updates TrackCreateScreen by extracting useLocation logic to a custom hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync
+} from 'expo-location';
+
+export default cb => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        cb
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [err];
+};
