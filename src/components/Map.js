9ef1b1ce20e86a5d736939de50ff2e52cda3188e
petercolella <peter.joseph.colella@gmail.com>
2020-07-23 15:39:52 -0400

    updates Map component with Circle component in place of region prop

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ActivityIndicator, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -21,12 +21,14 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-      }}
-    ></MapView>
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
