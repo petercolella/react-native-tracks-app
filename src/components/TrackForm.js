7980693922d287948e519c7d536cfb66d40dab26
petercolella <peter.joseph.colella@gmail.com>
2020-07-24 19:56:14 -0400

    adds LocationContext and relevant props in TrackForm

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Input } from 'react-native-elements';
 import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = () => {
+  const {
+    state: { locations, recording, trackName },
+    changeName,
+    startRecording,
+    stopRecording
+  } = useContext(LocationContext);
+
+  console.log(locations.length);
+
   return (
     <>
       <Spacer />
-      <Input placeholder="Enter Track Name" />
+      <Input
+        placeholder="Enter Track Name"
+        onChangeText={changeName}
+        value={trackName}
+      />
       <Spacer>
         <Button
-          title="Start Recording"
-          onPress={() => onSubmit({ email, password })}
+          title={recording ? 'Stop' : 'Start Recording'}
+          onPress={recording ? stopRecording : startRecording}
         />
       </Spacer>
     </>
