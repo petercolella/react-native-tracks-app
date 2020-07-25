168a8049a30386a4f05cb721b5768edf0a787a0d
petercolella <peter.joseph.colella@gmail.com>
2020-07-25 16:38:02 -0400

    removes recording context and adds startWatching function definition and cb dependency to useEffect in useLocation hook

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,46 +1,49 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Accuracy,
   requestPermissionsAsync,
   watchPositionAsync
 } from 'expo-location';
-import { Context as LocationContext } from '../context/LocationContext';
 
 export default (tracking, cb) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-  const {
-    state: { recording }
-  } = useContext(LocationContext);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      const subscriber = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        location => cb(location, recording)
-      );
-      setSubscriber(subscriber);
-    } catch (e) {
-      setErr(e);
-    }
-  };
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+        if (!granted) {
+          throw new Error('Location permission not granted');
+        }
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          cb
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (tracking) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
-  }, [recording, tracking]);
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [tracking, cb]);
 
   return [err];
 };
