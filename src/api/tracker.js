548dd4206767095ea9eeeb474bc3461270061612
petercolella <peter.joseph.colella@gmail.com>
2020-08-08 00:50:15 -0400

    adds token authorization header to axios instance in tracker.js

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://rn-track-server.herokuapp.com'
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  err => Promise.reject(err)
+);
+
+export default instance;
