e36097193e6c8b8cfb4146f303b745ceb9446860
petercolella <peter.joseph.colella@gmail.com>
2020-08-09 22:54:43 -0400

    adds TrackContext and Flatlist with TouchableOpacity and ListItem to TrackListScreen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { ListItem, Text } from 'react-native-elements';
+import { NavigationEvents } from 'react-navigation';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
   return (
-    <View>
-      <Text>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate('TrackDetail')}
+    <>
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text h2>Tracks</Text>
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem chevron title={item.name} />
+            </TouchableOpacity>
+          );
+        }}
       />
-    </View>
+    </>
   );
 };
 
